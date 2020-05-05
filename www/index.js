7a270ff60b9fe2f41314bdd3e2711d8ac75501d8
Jason Healy <jason@jhealy.net>
2020-05-05 17:06:02 -0600

    basic experience calculation functionality

--- www/index.js
+++ www/index.js
@@ -1,3 +1,10 @@
-import * as wasm from "hello-wasm-pack";
+import * as ffxiv from "ffxiv-squad-calc-wasm";
 
-wasm.greet();
+const currentLevel = document.getElementById('current-level');
+const expToNext = document.getElementById('exp-to-next');
+
+expToNext.innerText = ffxiv.calc_next_level_exp(currentLevel.value);
+
+currentLevel.addEventListener('change', () => {
+  expToNext.innerText = ffxiv.calc_next_level_exp(currentLevel.value);
+});
