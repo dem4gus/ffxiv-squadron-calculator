beaa54531507abc8c9d28d96fef11b730137a88b
Jason Healy <jason@jhealy.net>
2021-04-29 04:12:06 +0000

    fix copy config

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -9,6 +9,15 @@ module.exports = {
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'index.html',
+        }
+      ],
+    }),
   ],
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
